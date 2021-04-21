938736a379ff2b17ac9258f6f7b023ba28c8df7b
viranjali <barahate6767@gmail.com>
2021-04-21 13:14:50 +0530

    ballmovement

--- sketch.js
+++ sketch.js
@@ -1,29 +1,51 @@
-var ball;
+var ball, position;
+var database;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    //read ball's position from database
+    var ballPositionRef = database.ref('ball/position');
+    ballPositionRef.on("value",readPosition,showError);
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){   
+    database.ref('ball/position').set({
+        'x': position.x + x,  
+        'y': position.y + y    
+
+    })
+
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
+function showError(){
+    console.log("There is some error while reading value from the database");
 }
